868f8210b1197ec0e5c35bbd7e2f255419b63d4a
Dev-Nadson <nadson.pros@gmail.com>
2025-12-05 09:36:47 -0300

    Commit Inicial

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import { Footer } from "../Footer"
+import { Header } from "../Header"
+
+interface ILayoutProps {
+    children: React.ReactNode
+}
+
+function Layout({ children }: ILayoutProps) {
+    return (
+        <div className="relative flex min-h-screen flex-col dark">
+            <Header />
+            <main className="flex-1 flex flex-col mb-12">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export { Layout }
