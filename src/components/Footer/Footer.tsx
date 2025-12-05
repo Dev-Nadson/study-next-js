9482826d01a6d5b6b029f24a706c13c323ef160c
Dev-Nadson <nadson.pros@gmail.com>
2025-12-05 13:02:44 -0300

    Feat: footer concluído

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link"
+import Image from "next/image"
+
 function Footer() {
     return (
-        <footer>
-            <h2>Footer</h2>
+        <footer className="">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
+                <div className="flex justify-between md:flex-row gap-8 py-8">
+                    <Link href="/">
+                        <Image src="/logo.svg" alt="logo" width={116} height={32} />
+                    </Link>
+
+                    <nav className="flex flex-col md:flex-row items-start gap-4 text-sm text-muted-foreground">
+                        <Link href="/termos-de-uso" className="hover:text-primary">Termos de Uso</Link>
+                        <Link href="politica-de-privacidade" className="hover:text-primary">Política de Privacidade</Link>
+                        <Link href="feedback" className="hover:text-primary">Feedback</Link>
+                    </nav>
+                </div>
+            </div>
         </footer>
     )
 }
