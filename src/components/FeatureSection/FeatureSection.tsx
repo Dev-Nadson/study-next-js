fa42656f28968bb70136d39ae04699ae096c44d7
Dev-Nadson <nadson.pros@gmail.com>
2025-12-06 22:47:38 -0300

    Feat: Botão finalizado

--- src/components/FeatureSection/FeatureSection.tsx
+++ src/components/FeatureSection/FeatureSection.tsx
@@ -10,13 +10,15 @@ function FeatureSection() {
             <Container TagText="Simples" FeatureText="Crie um catálogo de produtos online em poucos minutos" />
             <Container TagText="Prático" FeatureText="Venda para seu público através de uma plataforma única" />
 
-            <div>
-                <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 rounded-lg bg-zinc-900 p-6 md:p-12 space-y-1 hover:scale-103 transition-all">
+            <div className="col-span-full flex flex-col gap-2">
+                <div className="grid space-y-10 grid-cols-1 md:grid-cols-2 md:gap-4 rounded-lg md:space-y-1 bg-zinc-900 p-6 md:p-12 hover:scale-103 transition-all">
                     <div className="flex flex-col gap-4 md:gap-0">
-                        <span className="text-blue-200 bg-blue-900 px-2 py-1 w-fit rounded-sm uppercase">Personalizável</span>
-                        <h2 className="text-2xl text-gray-100 font-bold">Tenha uma loja online personalizada com a cara da sua marca</h2>
+                        <div className="flex flex-col space-y-4">
+                            <span className="text-blue-200 bg-blue-900 px-2 py-1 w-fit rounded-sm uppercase">Personalizável</span>
+                            <h2 className="text-2xl text-gray-100 font-bold max-w-md">Tenha uma loja online personalizada com a cara da sua marca</h2>
+                        </div>
 
-                        <Button className=" items-center rounded-full mt-4 hidden md:mt-auto md:p-0 md:flex w-fit text-white bg-blue-500 hover:bg-blue-600" asChild >
+                        <Button className="items-center rounded-full mt-4 hidden md:mt-auto md:p-0 md:flex max-w-60 h-12 text-white text-lg bg-blue-500 hover:bg-blue-600" asChild >
                             <Link href="/criar-loja">
                                 Criar Loja Grátis
                                 <ArrowRight className="" />
@@ -26,7 +28,7 @@ function FeatureSection() {
 
                     <div className="flex flex-col items-center justify-center w-full ">
                         <div className="w-full max-w-md overflow-hidden">
-                            <Image src="./image.svg" alt="" width={440} height={330} className="object-cover w-full" />
+                            <Image src="./image.svg" alt="" width={440} height={330} className="object-cover w-full scale-110" />
                         </div>
                     </div>
 
