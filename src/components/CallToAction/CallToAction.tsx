97f68a0288cdb572cd299bf2255e9cf91e778e2d
Dev-Nadson <nadson.pros@gmail.com>
2025-12-11 22:42:22 -0300

    Fix: tamanho do texto corrigido

---
+++ src/components/CallToAction/CallToAction.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+import { Button } from "../ui/button"
+import { ArrowRight, Store } from "lucide-react"
+
+function CallToAction() {
+    return (
+        <section className="">
+            <div className="flex w-fit bg-teal-900 p-3 md:p-4.5 rounded-full m-auto relative translate-y-1/2">
+                <Store className="size-6 md:size-10 text-teal-300" />
+            </div>
+
+            <div className="flex min-h-65 flex-col gap-4 md:gap-12 py-8 bg-[url('/background-footer.svg')] bg-linear-to-b from-cyan-950/20 to-transparent px-5 md:py-40 items-center justify-center bg-cover bg-no-repeat bg-center">
+                <h2 className="text-2xl md:text-4xl font-bold text-center max-w-185">
+                    Crie uma loja online e inicie suas vendas ainda hoje!
+                </h2>
+
+                <Button className="bg-blue-500 hover:bg-blue-6 items-center rounded-full w-fit text-white" asChild >
+                    <Link href="/criar-loja">
+                        Criar Loja Grátis
+                        <ArrowRight className="" />
+                    </Link>
+                </Button>
+            </div>
+        </section>
+    )
+}
+
+export { CallToAction }
